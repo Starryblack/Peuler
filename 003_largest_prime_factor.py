093c59eef59e003fd612b00110cfd1ec09c2d6ea
WMS <mingsong123@gmail.com>
2018-06-22 00:11:00 +0800

    3rd commit, optimised for SPEED

--- 003_largest_prime_factor.py
+++ 003_largest_prime_factor.py
@@ -8,15 +8,18 @@ q=x
 
 for y in range(2, (x//2)+1):
 
-	temp=0
-	while q%y==0:
-		q=q/y
-		temp=temp+1
+	if y <= q:
+		temp=0
+		while q%y==0:
+			q=q/y
+			temp=temp+1
 	
-	if temp > 0:
-		lol=y
-
+		if temp > 0:
+			lol=y
+			print(lol)
 		
+	else:
+		break
 print(lol)
 
 
